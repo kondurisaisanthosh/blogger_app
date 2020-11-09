f734cf13396bfbc2f1ecb4e50bffffb6349597ea
kondurisaisanthosh <konduri.santosh96@gmail.com>
2020-11-08 22:30:04 -0500

    blogs

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title=models.CharField(max_length=100)
@@ -8,7 +9,6 @@ class Post(models.Model):
     # time and date at which it is created but can change dates
     date=models.DateTimeField(default=timezone.now)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
-
 
     # last modified date
     # date =models.DateTimeField(auto_now=true)
@@ -18,3 +18,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk':self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-
+from django.views.generic import ListView,DeleteView,DetailView,CreateView,UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 # posts=[
 #     {
@@ -10,28 +12,74 @@ from django.contrib.auth.decorators import login_required
 #         'title' : 'post1',
 #         'content' : 'first post content',
 #         'date' : 'august 10'
-#     },
-#     {
-#         'author' : 'santosh',
-#         'title' : 'post2',
-#         'content' : 'second post content',
-#         'date' : 'august 11'
-#     },
-#     {
-#         'author' : 'sandeep',
-#         'title' : 'post3',
-#         'content' : 'third post content',
-#         'date' : 'august 12'
 #     }
 # ]
 
-@login_required
-def home(request):
-    # return HttpResponse('<h1>Hello World</h1>')
-    context={
-        'posts':Post.objects.all()
-    }
-    return render(request,'blog/home.html',context)
+# @login_required
+# def home(request):
+#     # return HttpResponse('<h1>Hello World</h1>')
+#     context={
+#         'posts':Post.objects.all()
+#     }
+#     return render(request,'blog/home.html',context)
+
+class PostListView(LoginRequiredMixin,ListView):
+    model=Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date']
+    paginate_by = 4
+# <app>/<model>_<viewtype>.html=blog/post_list.html
+
+class UserPostListView(LoginRequiredMixin,ListView):
+    model=Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_queryset(self):
+        user=get_object_or_404(User,username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date')
+
+
+
+class PostDetailView(LoginRequiredMixin,DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user==post.author:
+            return True
+        else:
+            return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/home'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
 
 def myposts(request):
     return render(request,'blog/myposts.html',{'title':'myposts'})
