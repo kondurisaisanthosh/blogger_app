9825c7907e0f6bec8483769042921f430808740d
kondurisaisanthosh <konduri.santosh96@gmail.com>
2020-11-20 05:18:16 -0500

    forgot password

--- demo1/settings.py
+++ demo1/settings.py
@@ -131,3 +131,10 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_URL= 'login'
 
 LOGIN_REDIRECT_URL='blog-home'
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='konduri.santhosh96@gmail.com'
+EMAIL_HOST_PASSWORD='saisantosh1'
